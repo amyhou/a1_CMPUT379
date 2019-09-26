19d0e705e2bf59da4dc94a41414e5d293f36e198
Amy Hou <amyhou98@gmail.com>
2019-09-25 19:20:33 -0600

    fix changes to prev commit

--- dragonshell.c
+++ dragonshell.c
@@ -5,7 +5,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <limits.h>
-#include <stdlib.h>
 
 #define TRUE  (1)
 #define FALSE (0)
@@ -32,15 +31,25 @@ int main(int argc, char **argv) {
   // do this in a loop
   // program running flag
   int prog = TRUE;
+
+  /* Print welcome message */
+  printf("Welcome to DragonShell!\n");
+  printf("-------------------------\n");
+
   while (prog == TRUE)
   {
-    char input[PATH_MAX];
-    char * tokArgs[PATH_MAX];
+    char input[PATH_MAX] = "";
+    char * tokArgs[PATH_MAX] = {NULL};
     
     // print string prompt
     printf("dragonshell > ");
     // get input
-    scanf("%[^\n]%*c", &input[0]);
+    if (scanf("%[^\n]%*c", &input[0]) != 1)
+    {
+      printf("\ndragonshell: Failed to read from stdin.\n");
+      fflush(stdin);
+      continue;
+    }
 
     // tokenize into separate commands using delimiter ';'
     tokenize(&input[0], ";", &tokArgs[0]);
@@ -49,7 +58,7 @@ int main(int argc, char **argv) {
     while (tokArgs[i] != NULL)
     {
       // tokenize into separate commands/arguments using delimiter ' '
-      char * cmdArgs[PATH_MAX];
+      char * cmdArgs[PATH_MAX] = {NULL};
       printf("tokArgs[i]: %s\n", tokArgs[i]);
       fflush(stdout);
 
@@ -57,12 +66,16 @@ int main(int argc, char **argv) {
       
       printf("cmdargs[0]: %s, cmdargs[1]: %s\n", cmdArgs[0], cmdArgs[1]);
       fflush(stdout);
-      if (strcmp(cmdArgs[0], "exit") == 0)
+      if (strcmp(cmdArgs[0], "exit") == 0) // exit dragonshell
       {
         prog = FALSE;
         break;
       }
-      break;
+      else // command not found
+      {
+        printf("\ndragonshell: Command not found\n");
+        break;
+      }
       i++;
     }
     fflush(stdout);
